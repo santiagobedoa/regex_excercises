d6105e63868efd496204359d453a90fe5b18e210
Santiago Bedoya <santiagobedoa@gmail.com>
2022-06-29 14:08:31 -0500

    Wed Jun 29 14:08:31 -05 2022

--- mail_checker.rb
+++ mail_checker.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 # regex that checks for a valid mail
-puts ARGV[0].scan(/\S+@\S+\.[A-Za-z]+\.?[A-Za-z]*/).join
+puts ARGV[0].scan(/[^@^\s]+@[^@^\s]+\.[A-Za-z]+\.?[A-Za-z]*/).join
